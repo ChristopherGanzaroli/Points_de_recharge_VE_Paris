cd81398dfb85d2e45b7b04dcb18c96d7873cbc91
Christopher Ganzaroli <60617045+ChristopherGanzaroli@users.noreply.github.com>
2022-04-18 00:25:43 +0200

    modification histogram and markers map

--- bornes_charge_ve_paris.py
+++ bornes_charge_ve_paris.py
@@ -1,3 +1,5 @@
+from cmath import nan
+from matplotlib.pyplot import axis
 import requests
 import json
 import csv
@@ -7,45 +9,43 @@ from datetime import datetime
 import datetime
 import plotly.express as px
 import plotly.graph_objects as go
-# In[2]:
+import dash
+#import dash_leaflet as dl
+#pd.set_option('display.max_rows', 10)
+########################################################################################################################
+# IMPORTATION ET LECTURE DES DONNEES EN TEMPS REEL DEPUIS L'API OPEN DATA PARIS
+########################################################################################################################
 
-
-pd.set_option('display.max_rows', 10)
-
-# In[3]:
-
-
+# L'ecture de l'url dans lesquels les données sont présentes
 r = requests.get("https://opendata.paris.fr/api/records/1.0/search/?dataset=belib-points-de-recharge-pour-vehicules-electriques-disponibilite-temps-reel&q=&rows=9999&sort=code_insee_commune&facet=statut_pdc&facet=last_updated&facet=arrondissement")
+#r.encoding
+#Création d'un objet data contenant les données de l'url
 data = r.json()
-json.dumps(data, indent=4, sort_keys=False)
-
-# print(data)
-
-# In[4]:
+#Dans l'objet data on filtre sur "records" pour uniquement récupérer les données 
+sites = data["records"]
 
 path = "/Users/ganza/OneDrive/Bureau/Projet_prerso/python/pythonProject/pythonProject/bornes_recharge_application_dash/data"
 path_token = "/Users/ganza/OneDrive/Bureau/Projet_prerso/python/pythonProject/pythonProject/bornes_recharge_application_dash"
 
-# In[7]:
+########################################################################################################################
+# ENREGISTREMENT ET MISE EN FORME DES DONNEES
+########################################################################################################################
+"""
+Création d'un ficher en mode écriture "w" s'il n'existe pas afin d'enregistrer les données.
+with open clot automatiquement le fichier après instruction.
+"""
 
-
-
-
-
-#with open(path + '/data.json', 'w') as json_file:
-#     json.dump(data, json_file, indent=4, sort_keys=False)
-
-# # In[8]:
-
-sites = data['records']
-
+#création du chemin vers lequel pointeront les données
 with open(path + '/stations.csv', 'w', newline='', encoding='utf-8') as file:
+    #Ecrire dans le ficher avec les noms de colonnes
     writer = csv.writer(file,delimiter=',')
+    #on crait les colonnes du fichier
     writer.writerow(['adresse_station', 'arrondissement', 'statut_pdc', 'code_insee_commune', 'coordonneesxy', 'lat', 'long','id_pdc', 'last_date_time_update'])
+    #on itère dans l'objet data pour récuperer les données présentes dans le fields (un niveau sous records)
     try :
         for elt in sites:
             adress = elt["fields"]["adresse_station"]
-            print(adress)
+            #print(adress)
             arrondissement = elt['fields']['arrondissement']
             # print(arrondissement)
             statut = elt['fields']['statut_pdc']
@@ -62,125 +62,126 @@ with open(path + '/stations.csv', 'w', newline='', encoding='utf-8') as file:
             # print(id_pdc)
 
             date = elt['fields']['last_updated']
+            
             last_date_time_update = datetime.datetime.fromisoformat(date)
     #        last_date_update = last_date_time_update.date()
     #        last_time_update = last_date_time_update.time()
-
+            
+            #on remplit les colonnes avec les variables ci-dessus
             writer.writerow([adress, arrondissement, statut, cp, lat_long, lat, long, id_pdc, last_date_time_update])
     except KeyError:
         pass
-# Transform in pandas dataframe. Change index and transform in date format
+
+"""On récupère les données du fichier csv et on met les dates en index afin
+    d'en extraire plus facilement l'heure (utile pour l'histogram)
+"""
 df = pd.read_csv(path + '/stations.csv', index_col= 'last_date_time_update',parse_dates=True)
-
-
 print(df.head())
 
-# Get hour from index into new col
+# On crait une colonne heure
 df['last_update_by_hour'] = df.index.hour
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#nb_stations = dict(df['adresse_station'].value_counts())
-
-# Get number of station by site
+# Pour chaque station, on récupère le nombre de bornes disponible
 df['nb_charging_stations'] = df.groupby(by='adresse_station')['adresse_station'].transform('count')
 
+"""
+Pour chaque stations nous allons messurer son occupation.
 
+"""
+def statut_count(df):
+    
+        if  df.statut_pdc == "Occupé (en charge)" :
+            return 1
+        else:
+            None
+
+df["occupation (1=yes, 0=no)"] = df.apply(statut_count, axis=1)
+
+df["occupation (1=yes, 0=no)"] = df.groupby(by="adresse_station")["occupation (1=yes, 0=no)"].transform('count')
+
+
+
+"""
+Pour chaque arrondissements, nous voulons messurer le taux d'occupation des stations de recharges
+"""
+#dictionnaire comptenant le nombre de bornes occupées pour chaque arrondissements
+from collections import defaultdict
+d = defaultdict(list)
+for k,v in zip(df.code_insee_commune,df["occupation (1=yes, 0=no)"]):
+    if v == 1:
+        d[k].append(v)
+       
+for k,v in d.items():
+    d[k] = sum(v)
+
+    
+        
 # https://opendata.paris.fr/explore/dataset/belib-points-de-recharge-pour-vehicules-electriques-disponibilite-temps-reel/table/?disjunctive.statut_pdc&disjunctive.arrondissement&sort=id_pdc&dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJjb2x1bW4iLCJmdW5jIjoiQ09VTlQiLCJzY2llbnRpZmljRGlzcGxheSI6dHJ1ZSwiY29sb3IiOiIjRkZDRDAwIn1dLCJ4QXhpcyI6Imxhc3RfdXBkYXRlZCIsIm1heHBvaW50cyI6IiIsInRpbWVzY2FsZSI6ImhvdXIiLCJzb3J0IjoiIiwiY29uZmlnIjp7ImRhdGFzZXQiOiJiZWxpYi1wb2ludHMtZGUtcmVjaGFyZ2UtcG91ci12ZWhpY3VsZXMtZWxlY3RyaXF1ZXMtZGlzcG9uaWJpbGl0ZS10ZW1wcy1yZWVsIiwib3B0aW9ucyI6eyJkaXNqdW5jdGl2ZS5zdGF0dXRfcGRjIjp0cnVlLCJkaXNqdW5jdGl2ZS5hcnJvbmRpc3NlbWVudCI6dHJ1ZSwic29ydCI6ImNvZGVfaW5zZWVfY29tbXVuZSJ9fX1dLCJkaXNwbGF5TGVnZW5kIjp0cnVlLCJhbGlnbk1vbnRoIjp0cnVlfQ%3D%3D&rows=999&timezone=&basemap=jawg.dark&location=12,48.86471,2.33974
 
+########################################################################################################################
+# CREATION DE LA MAP ET DES GRAPH INTERACTIFS
+########################################################################################################################
 
-#CREATION GRPH---------------------------------------------------------------------------------------------------
-#MAP
+"""
+Création de la web app avec la librairie Dash (pour la web app intéractive)
+et plotly (pour la création de graphiques)
+"""
+colorscale=[
+             [0, "#F4EC15"],
+             [0.04167, "#DAF017"],
+             [0.0833, "#BBEC19"],
+             [0.125, "#9DE81B"],
+             [0.1667, "#80E41D"],
+             [0.2083, "#66E01F"],
+             [0.25, "#4CDC20"],
+             [0.292, "#34D822"],
+             [0.333, "#24D249"],
+             [0.375, "#25D042"],
+             [0.4167, "#26CC58"],
+             [0.4583, "#28C86D"],
+             [0.50, "#29C481"],
+             [0.54167, "#2AC093"],
+             [0.5833, "#2BBCA4"],
+             [1.0, "#613099"],
+             ]
+#On génère une map avec mapbox depuis la librairie plotly
 mapbox_access_token = px.set_mapbox_access_token(open(path_token + "/mapbox_token.txt").read())
-fig = px.scatter_mapbox(df, lat='lat', lon='long', color='statut_pdc', size='nb_charging_stations',
-                        mapbox_style='carto-darkmatter', animation_group='adresse_station',opacity=0.70,
-                        labels='nb_charging_stations',
+fig = px.scatter_mapbox(df, lat='lat', lon='long', size=(df["occupation (1=yes, 0=no)"]/df["nb_charging_stations"])*100,
+                        mapbox_style='carto-darkmatter', animation_group='adresse_station',opacity=0.20,
+                        labels='nb_charging_stations',hover_data=['nb_charging_stations'],
                         # hover_data=[df['statut_pdc'][i] for i in range(df.shape[0])],
-                        color_continuous_scale=px.colors.cyclical.IceFire, size_max=7, zoom=11.2)
+                        color_continuous_scale=px.colors.cyclical.IceFire, size_max=12, zoom=11.2)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-# colorscale=[
-#             [0, "#F4EC15"],
-#             [0.04167, "#DAF017"],
-#             [0.0833, "#BBEC19"],
-#             [0.125, "#9DE81B"],
-#             [0.1667, "#80E41D"],
-#             [0.2083, "#66E01F"],
-#             [0.25, "#4CDC20"],
-#             [0.292, "#34D822"],
-#             [0.333, "#24D249"],
-#             [0.375, "#25D042"],
-#             [0.4167, "#26CC58"],
-#             [0.4583, "#28C86D"],
-#             [0.50, "#29C481"],
-#             [0.54167, "#2AC093"],
-#             [0.5833, "#2BBCA4"],
-#             [1.0, "#613099"],
-#             ]
-# #Attribut color for inch statut for mapbox
-# color = np.append(np.insert(df.index.hour, 0, 0), 3)
-# print(color)
-#
-#
-# fig = go.Figure(go.Scattermapbox(
-#             lat=df["lat"],
-#             lon=df["long"],
-#             mode='markers',
-#         marker=go.scattermapbox.Marker(dict(
-#                         #colorscale=color_statut,
-#                         size=9,
-#                         color=np.append(np.insert(df.index.hour, 0, 0), 24),
-#
-#                                 )
-#
-#                             ),
-#         text=df['statut_pdc'],
-#     ))
-#
-# fig.update_layout(
-#     autosize=True,
-#     hovermode='closest',
-#     showlegend=True,
-#
-#     mapbox=dict(
-#         accesstoken="pk.eyJ1IjoiY2hyaXN0b3BoZXJnYW56YXJvbGkiLCJhIjoiY2t6bHN4Y2t6MmhkdDJwbzBoMGo4bDkyMSJ9.dHqwH6gzC1o8V24zUrTwIg",
-#         bearing=0,
-#
-#         style="dark",
-#         center=dict(
-#             lat=48.86409,
-#             lon=2.3437343
-#
-#         ),
-#         pitch=0,
-#         zoom=11.2,
-#
-#     ),
-# )
+#fig.update_traces(hovertemplate=None)
+fig.update_coloraxes(colorscale=colorscale)
 
 
-#HISTOGRAM
-fig_hist = px.histogram(df, x="last_update_by_hour", nbins=24)
-# fig_hist = px.histogram(df, x="last_update_by_hour", y="statut_pdc", histfunc="count", nbins=24, text_auto=True)
 
 
+############################### HISTOGRAM ###############################
+'''
+On cherche ici à afficher l'occupation des bornes de recharge.
+On va donc créer une nouvelle colonnes afin d'extraire si "oui" ou non la station est disponible.
+si "oui", on récupère l'heure
+
+'''
+
+nb_disponibility_station_by_hours = []
+for a,b in zip(df.last_update_by_hour, df["occupation (1=yes, 0=no)"]):
+    if b == 1:
+        nb_disponibility_station_by_hours.append(a)
+
+hours_hist = ([i for i in df.last_update_by_hour] + [i for i in range(0,24) if i not in nb_disponibility_station_by_hours])
+fig_hist = px.histogram(df, x=hours_hist,text_auto=True)
+#Espacer les bars
+fig_hist.update_layout(bargap=0.01,showlegend=False)
+
+
+
+
+###################### PROBLEMES ######################
+#Pourquoi y a-t'il tant d'occupation à 2h ?
+#print(hours_hist[hours_hist == 2])
 
 
 
